152079ef9f3f68359ad6333bb6f52748f0a283dd
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-09-05 20:36:54 +0545

    new a

--- src/Pages/UpdateProfile.jsx
+++ src/Pages/UpdateProfile.jsx
@@ -1,6 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function UpdateProfile() {
+
+  const [loading, setLoading] = useState(false)
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    businessname: "",
+    image: null
+  })
+  const [formErrors, setFormErrors] = useState([]);
+
+  function handleChange(e){
+    const {name, value, files} = e.target;
+    if (name === "image") {
+      setUserData({ ...userData, image: files[0] });
+    } else {
+      setMenuData({ ...userData, [name]: value });
+    }
+  }
+
+let userId = localStorage.getItem("userId")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('businessname', userData.businessname);
+    if (userData.image) {
+      formData.append('image', userData.image);
+    }
+    axios
+      .post(`https://himalayanjava-server.onrender.com/api/update/${userId}`, formData,
+    )
+      .then((res) => {
+        toast("Profile updated successfully!", { autoClose: 2000 });
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        let errors = err.response?.data?.errors;
+        if (err?.response?.status === 400 && errors) {
+          setFormErrors(errors);
+        } else {
+          toast.error("Something went wrong", { autoClose: 2000 });
+        }
+      });
+  }
+
   return (
     <div className="md:px-10 pb-5">
       <h2 className="md:text-center text-2xl text-amber-900 font-semibold my-5">
@@ -12,7 +63,7 @@ function UpdateProfile() {
           <h2 className="text-center text-lg w-36">User01</h2>
         </div>
         <div className="w-full md:w-[55%]">
-        <form className="w-full">
+        <form onSubmit={handleSubmit} className="w-full">
       <label htmlFor="name">
         Username*
       </label>
@@ -21,7 +72,6 @@ function UpdateProfile() {
         id="name"
         name="name"
         type="text"
-        required
       />
       <label htmlFor="price">
         Email*
@@ -30,9 +80,7 @@ function UpdateProfile() {
         className="p-2 w-full border-[1px] border-[#C2C5E1] h-8 text-sm rounded-md"
         name="price"
         type="text"
-        min={0}
         id="price"
-        required
       />
       <label htmlFor="price">
         Password*
@@ -41,20 +89,18 @@ function UpdateProfile() {
         className="p-2 w-full border-[1px] border-[#C2C5E1] h-8 text-sm rounded-md"
         name="price"
         type="text"
-        min={0}
         id="price"
-        required
       />
-      <label htmlFor="price">
+      <label htmlFor="businessname">
         Business name*
       </label>
       <input
         className="p-2 w-full border-[1px] border-[#C2C5E1] h-8 text-sm rounded-md"
-        name="price"
+        name="businessname"
         type="text"
-        min={0}
-        id="price"
-        required
+        id="businessname"
+        value={userData.businessname}
+        onChange={handleChange}
       />
       <label htmlFor="image">
         Profile Picture
@@ -64,8 +110,9 @@ function UpdateProfile() {
         name="image"
         type="file"
         id="image"
+        onChange={handleChange}
       />
-      <button className="bg-[#D8C3A5] text-amber-900 px-3 py-2 mt-5 text-[16px] rounded-lg">
+      <button disabled={loading} className="disabled:bg-[#dad7d3] disabled:cursor-no-drop bg-[#D8C3A5] text-amber-900 px-3 py-2 mt-5 text-[16px] rounded-lg">
         Save data
       </button>
     </form>
