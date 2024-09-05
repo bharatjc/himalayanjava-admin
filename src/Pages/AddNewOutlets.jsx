36e7422521e1b56c1a979620d9870fd71c3731e3
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-09-05 09:21:58 +0545

    outlet added

--- src/Pages/AddNewOutlets.jsx
+++ src/Pages/AddNewOutlets.jsx
@@ -1,31 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddNewOutlets() {
+
+  const [loading, setLoading] = useState(false)
+  const [outletdata, setOutletData] = useState({
+    location: "",
+    special: "",
+    image:null
+  })
+  const [formErrors, setFormErrors] = useState([]);
+
+  function handleChange(e){
+    const {name, value, files} = e.target;
+    if (name === "image") {
+      setOutletData({ ...outletdata, image: files[0] });
+    } else {
+      setOutletData({ ...outletdata, [name]: value });
+    }
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('location', outletdata.location);
+    formData.append('special', outletdata.special);
+    if (outletdata.image) {
+      formData.append('image', outletdata.image);
+    }
+    axios
+      .post(`https://himalayanjava-server.onrender.com/outlet`, formData,
+      {headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },}
+    )
+      .then((res) => {
+        toast("New outlet added successfully!", { autoClose: 2000 });
+        setOutletData({
+          location: "",
+          special: "",
+          image: null
+        })
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        let errors = err.response?.data?.errors;
+        if (err?.response?.status === 400 && errors) {
+          setFormErrors(errors);
+        } else {
+          toast.error("Something went wrong", { autoClose: 2000 });
+        }
+      });
+  }
+
   return (
     <div>
     <h2 className="md:text-center text-2xl text-amber-900 font-semibold my-5">
           Add new outlet
           </h2>
-          <form className="mt-10 text-lg md:px-40">
-    <label htmlFor="name" className="font-semibold">
+          <form onSubmit={handleSubmit(e)} className="mt-10 text-lg md:px-40">
+    <label htmlFor="location" className="font-semibold">
       Location*
     </label>
     <input
       className="mb-5 p-2 w-full border-[1px] border-[#C2C5E1] h-8 text-sm"
-      id="name"
-      name="name"
+      id="location"
+      name="location"
       type="text"
       required
+      value={outletdata.location}
+      onChange={handleChange}
     />
-    <label htmlFor="price" className="font-semibold">
+    <label htmlFor="special" className="font-semibold">
       Special
     </label>
     <input
       className="mb-5 p-2 w-full border-[1px] border-[#C2C5E1] h-8 text-sm"
-      name="price"
+      name="special"
       type="text"
-      min={0}
-      id="price"
+      id="special"
+      value={outletdata.special}
+      onChange={handleChange}
     />
     <label htmlFor="image" className="font-semibold">
       Image
@@ -35,8 +94,9 @@ function AddNewOutlets() {
       name="image"
       type="file"
       id="image"
+      onChange={handleChange}
     />
-    <button className="bg-[#D8C3A5] text-amber-900 px-3 py-2 mt-5 text-[16px] rounded-lg">
+    <button disabled={loading} className="disabled:bg-[#dad7d3] disabled:cursor-no-drop bg-[#D8C3A5] text-amber-900 px-3 py-2 mt-5 text-[16px] rounded-lg">
       Add Location
     </button>
   </form>
